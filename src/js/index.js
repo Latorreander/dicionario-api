aebf45d629dab89003ec32b081a06ae9c73d83fd
Latorreander <latorreander@gmail.com>
2023-03-02 17:20:58 -0300

    'responsividade e tratativa de erros no js'

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const button = document.getElementById("search-button");
+        button.click();
+    }
+});
+
+async function wordSearch(wordToBeSearch) {
+    const url = `https://api.dicionario-aberto.net/word/${wordToBeSearch}/1`;
+    const response = await fetch(url);
+    const searchResult = await response.json();
+
+    if(wordToBeSearch === ''){
+        return alert('VOCÊ PRECISA DIGITAR UMA PALAVRA!')
+    }
+    try{
+        if(wordToBeSearch !== searchResult[0].word){
+           
+        }
+    }
+    catch{
+         alert('PALAVRA INEXISTENTE!')
+    }
+
+    
+    const answerArea = document.getElementsByClassName("search-result");
+    
+    answerArea[0].innerHTML = `<p>${searchResult[0].xml}</p>`;
+}
+
+async function search() {
+    let input = document.getElementsByName("input-word");
+    let wordToBeSearch = input[0].value;
+
+    wordSearch(wordToBeSearch);
+}
+
